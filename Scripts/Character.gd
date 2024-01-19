e57963107251b2c7a24105b1af1c37492b8e2930
Jacob <jacobmckeegames@gmail.com>
2024-01-19 00:21:57 -0600

    Stuff

--- Scripts/Character.gd
+++ Scripts/Character.gd
@@ -13,6 +13,10 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var model = $Knight
 @onready var animation_tree = $AnimationTree
 
+var attack_combo = ["Attack1", "Attack2", "Attack3"]
+var attacking = false
+var attack_index = 0
+
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
 		camera.rotation.x -= event.relative.y * mouse_sensitivity
@@ -21,19 +25,19 @@ func _unhandled_input(event):
 
 
 func _physics_process(delta):
-	# Add the gravity.
+	# Add the gravity and handle jump/fall animations
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 		animation_tree["parameters/Fall/blend_amount"] = lerp(animation_tree["parameters/Fall/blend_amount"], 1.0, 0.2)
 	else:
 		animation_tree["parameters/Fall/blend_amount"] = lerp(animation_tree["parameters/Fall/blend_amount"], 0.0, 0.2)
 
-	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	
+
+	# Directional input and movement animation
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized().rotated(Vector3.UP, camera.rotation.y)
 	animation_tree["parameters/IdleRunning/blend_amount"] = lerp(animation_tree["parameters/IdleRunning/blend_amount"], direction.length(), 0.1)
@@ -43,6 +47,17 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	if Input.is_action_just_pressed("attack") and is_on_floor():
+		if attacking:
+			attack_index += 1
+			if attack_index >= attack_combo.size():
+				attack_index = 0
+				attacking = false
+			animation_tree.set("parameters/" + attack_combo[attack_index] + "/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+		else:
+			animation_tree.set("parameters/" + attack_combo[attack_index] + "/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+			attacking = true
 
 	move_and_slide()
 
